0a0bab0cf6366da1eb3cc98215fe5fd5cdd07deb
Mike Potter <mpotter.drupal@gmail.com>
2019-02-04 16:27:26 -0500

    Add particle install to build

--- bin/build.sh
+++ bin/build.sh
@@ -5,6 +5,14 @@ composer clear-cache
 COMPOSER_PROCESS_TIMEOUT=2000 COMPOSER_DISCARD_CHANGES=1 composer install
 
 # Build the theme
-cd src/themes/particle && npm run build:drupal
+THEME_PATH="src/themes/particle"
+if [ -e ${THEME_PATH} ]; then
+  # Theme exists, so rebuild it.
+  cd ${THEME_PATH} && npm run build:drupal
+else
+  # If theme doesn't exist, go install Particle
+  bin/particle.sh
+fi
+
 cd ${CURRENT_DIR}
 
