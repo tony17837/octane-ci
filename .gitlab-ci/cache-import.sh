3f2f32a81f860b291b1055fb55fa506967ab82b8
Mike Potter <mpotter.drupal@gmail.com>
2019-02-22 15:55:29 -0500

    Test rsync

--- .gitlab-ci/cache-import.sh
+++ .gitlab-ci/cache-import.sh
@@ -9,35 +9,12 @@ cd /var/www
 # Copy any files needed from the container into /build.
 # For example, vendor, node_modules, etc.
 
-##rsync -au . /build
+rsync -au . /build
 # Make directories writeable so docker can access.
-##find /build -type d -exec chmod a+w {} \;
+find /build -type d -exec chmod a+w {} \;
 # Make files writeable so docker can access.
-##find /build -type f -exec chmod a+w {} \;
+find /build -type f -exec chmod a+w {} \;
 
-# Only copy cached vendor if project doesn't already have one in repo.
-if [[ -e vendor  && ! -e /build/vendor ]]; then
-  cp -R vendor /build
-  # Make directories writeable so docker can access.
-  find /build/vendor -type d -exec chmod a+w {} \;
-  # Make files writeable so docker can access.
-  find /build/vendor -type f -exec chmod a+w {} \;
-  ls -al /build
-  ls -al /build/vendor/composer/
-  ls -al /build/vendor/bin
-  ls -al /build/vendor/squizlabs/php_codesniffer/bin
-fi
-
-# Only copy cached composer.lock if project doesn't already have one in repo.
-if [[ -e composer.lock && ! -e /build/composer.lock ]]; then
-  cp composer.lock /build
-fi
-
-# Only copy cached node_modules if project doesn't already have one in repo.
-if [[ -e ${THEME_PATH}/node_modules && ! -e ${THEME_PATH}/node_modules ]]; then
-  cp -R ${THEME_PATH}/node_modules /build/${THEME_PATH}/node_modules
-  # Make directories writeable so docker can access.
-  find /build/${THEME_PATH}/node_modules -type d -exec chmod a+w {} \;
-  # Make files writeable so docker can access.
-  find /build/${THEME_PATH}/node_modules -type f -exec chmod a+w {} \;
-fi
+ls -al /build
+ls -al /build/vendor
+ls -al /build/build/docroot
