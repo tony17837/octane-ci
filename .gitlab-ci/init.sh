0b8299955a48a3b6f85ca83947f0d49bb1fd4d21
Mike Potter <mpotter.drupal@gmail.com>
2019-02-22 10:20:03 -0500

    Test raw docker

--- .gitlab-ci/init.sh
+++ .gitlab-ci/init.sh
@@ -2,7 +2,7 @@
 # Script used in GitLab CI to build the site from scratch.
 # Should match the .octane/init.sh script used for Docksal local setup.
 
-cd /var/www
+#cd /var/www
 ls -al
 
 # Install Particle
