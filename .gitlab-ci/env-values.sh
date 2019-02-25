51054fe2e834bd02b0a904cd818ecbd3db38ebde
Mike Potter <mpotter.drupal@gmail.com>
2019-02-25 13:35:44 -0500

    Set values.yaml from .env variables

---
+++ .gitlab-ci/env-values.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# Sets chart values from environment variables.
+
+# Read environment variables from .env file, skipping comments.
+export $(grep -v '^#' .env | xargs)
+
+# Output the environment variables into YAML format.
+echo "env:"
+echo "  projectName: ${PROJECT_NAME}"
+echo "  profile: ${PROFILE}"
+echo "  docroot: /var/www/${DOCROOT}"
+echo "  projectDir: ${PROJECT_DIR}"
+echo "  configPath: ${CONFIG_PATH}"
+echo "  themePath: ${THEME_PATH}"
+echo "mariadb:"
+echo "  mariadbUser: ${MYSQL_USER}"
+echo "  mariadbPassword: ${MYSQL_PASSWORD}"
+echo "  mariadbDatabase: ${MYSQL_DATABASE}"
+
