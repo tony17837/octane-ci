e9ee5bf383b0dc9e3686070fc83ac00524da86a9
Mike Potter <mpotter.drupal@gmail.com>
2019-03-12 13:31:41 -0400

    Convert to using configMap

--- .gitlab-ci/cache-prime.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Called from .gitlab-ci.yml when the build cache should be loaded.
-# Build cache copied from the previous build container
-
-# Login so we can push images to the registry
-docker login -u gitlab-ci-token -p $CI_JOB_TOKEN $CI_REGISTRY
-# Grab cached files from previous build.
-docker run --rm -v ${PWD}:/build ${WEB_IMAGE}:${CI_COMMIT_REF_SLUG} /build/.gitlab-ci/cache-import.sh
