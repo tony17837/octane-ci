8472d8c660caa5237f3306743963870bd8d7e804
Mike Potter <mpotter.drupal@gmail.com>
2019-03-12 09:52:32 -0400

    Merge common init script

--- bin/init.sh
+++ bin/init.sh
@@ -3,10 +3,20 @@
 # This script is called by the Docksal "fin init" to set up local environment.
 # This script runs within the Build container, so all tools are available.
 
-printf "$INFO_SLUG Installing Particle...\n"
-./bin/particle
+if [ -e "./bin/particle" ]; then
+  printf "$INFO_SLUG Installing Particle...\n"
+  # Install Particle
+  ./bin/particle
+  retVal=$?
+  if [ $retVal -ne 0 ]; then
+    # Exit if particle failed.
+    exit $retVal
+  fi
+fi
 
 printf "$INFO_SLUG Building site...\n"
 # Initial build of site.
 ./bin/build "$@"
+# Pass the install error status back out to fail job if build fails.
+exit $?
 
