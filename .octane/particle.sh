2370ad19d67d0b17a778718835185788e4806d69
Mike Potter <mpotter.drupal@gmail.com>
2019-05-13 14:50:03 -0400

    Fix particle install in fresh project

--- .octane/particle.sh
+++ .octane/particle.sh
@@ -10,11 +10,14 @@ if [ -e ~/.profile ]; then
 fi
 
 # Only download particle theme if it doesn't already exist.
-# Can't just check for ${THEME_PATH} since node_modules might be cached.
-# So count the number of files in the theme to see if there are more than
-# just cached node_modules and package-lock.json.
-# Exclude . and .. when counting (using -A flag)
-FILE_COUNT=`ls -A1 ${THEME_PATH} | wc -l | tr -d ' '`
+FILE_COUNT=0
+if [ -d ${THEME_PATH} ]; then
+  # Can't just check for ${THEME_PATH} since node_modules might be cached.
+  # So count the number of files in the theme to see if there are more than
+  # just cached node_modules and package-lock.json.
+  # Exclude . and .. when counting (using -A flag)
+  FILE_COUNT=`ls -A1 ${THEME_PATH} | wc -l | tr -d ' '`
+fi
 if [ $FILE_COUNT -le 2 ]; then
   printf "$INFO_SLUG Downloading and installing Particle...\n"
   # Clear the directory for npx to install.
